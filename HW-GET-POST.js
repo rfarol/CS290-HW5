67ccafc0f0e84f5e10414e5f9ff91b0c4b9c8be7
rfarol <farolr@oregonstate.edu>
2020-11-16 22:40:41 -0800

    HW5 UPDATED

--- HW-GET-POST.js
+++ HW-GET-POST.js
@@ -11,27 +11,33 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 4242);
 
+/* get form calls the handlebar: get-request-output to render. Push method was taken from lecture week 8 - Server Side form Handling.
+Takes the two names: label and key, and makes an array object from the query. It can be iterated and output the labels and keys of the 
+elements within the list. This way get-request-output.handlebars can access each label name by using this.label */
 app.get('/get-request-output',function(req,res){
-  var getquerylist = [];
+  var getquerylist = []; // intialize empty array
   for (var i in req.query){
-    getquerylist.push({'label':i,'key':req.query[i]})
+    getquerylist.push({'label':i,'key':req.query[i]}) 
   }
   var context = {};
   context.getquerylist = getquerylist;
   res.render('get-request-output', context);
 });
 
+
+/* post form calls the handlebar: get-request-output to render. Push method was taken from lecture week 8 - Server Side form Handling.
+Takes the two names: label and key, and makes an array object from the query. It can be iterated and output the labels and keys of the 
+elements within the list. This way get-request-output.handlebars can access each label name by using this.label or this.blabel.
+Two lists were made. One for the query, and one for the body. */
 app.post('/post-request-output', function(req,res){
-  var postquerylist = [];
-  var postbodylist = [];
+  var postquerylist = []; // intialize empty array for the QUERY
+  var postbodylist = []; // Intialize empty array for the BODY
   for (var i in req.query){
     postquerylist.push({'label':i,'key':req.query[i]})
   }
   for (var i in req.body){
     postbodylist.push({'blabel':i,'bkey':req.body[i]})
   }
-  console.log(qParams);
-  console.log(req.body);
   var context = {};
   context.postquerylist = postquerylist;
   context.postbodylist = postbodylist;
